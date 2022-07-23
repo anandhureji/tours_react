a7d5fd7ef1ec1f78c62fe2c3d9dc1647aa9eee6f
Anandhu <anandhureji33@gmail.com>
2022-07-24 00:47:01 +0530

    Second commit

---
+++ tour/src/Tour.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,name,info,price,image}) => {
+
+  const [readMore,setReadmore] = useState(false);
+  return(
+    <article className='single-tour'>
+      <img src={image} alt={name}>
+      </img>
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>{readMore?info:`${info.substring(0,200)}...`}
+        <button onClick={()=> setReadmore(!readMore)}>
+          {readMore?'show less':'Read More'}
+        </button>
+        </p>
+        <button  className='delete-btn'>Not Intrested</button>
+      </footer>
+
+    </article>
+  );
+};
+
+export default Tour;
---
+++ tour/src/Tours.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours}) => {
+  return(
+    <section>
+      <div className='title'>
+        <h1>Our Tours</h1>
+        <div className='underline'></div>
+        </div>
+        <div>
+          {tours.map((tour)=>{
+            return <Tour key={tour.id} {...tour}></Tour>;
+          })}
+        </div>
+    </section>
+
+  );
+};
+
+export default Tours;
